a3d36d5fb70ae66b51a16c1311ccfd5ba23753de
5kdk <tylerk1121@gmail.com>
2023-06-13 20:40:06 +0900

    feat: 페이지 라우팅 구성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './styles';
+import { Root, SignIn, SignUp, Todo } from './pages';
+
+const routerConfig = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: 'signin',
+        element: <SignIn />,
+      },
+      {
+        path: 'signup',
+        element: <SignUp />,
+      },
+      {
+        path: 'todo',
+        element: <Todo />,
+      },
+    ],
+  },
+]);
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
+    <RouterProvider router={routerConfig} />
   </ThemeProvider>
 );
 
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Root } from './Root';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
+export { default as Todo } from './Todo';
