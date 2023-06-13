a51fb1e15b1ee092e40e360b080a3636144accca
5kdk <tylerk1121@gmail.com>
2023-06-14 01:45:37 +0900

    refactor: 상수 객체명 대문자로 변경

--- src/constants/serverPath.js
+++ src/constants/serverPath.js
@@ -1,8 +1,8 @@
-const serverPath = {
+const SERVER_PATH = {
   BASE_SERVER_URL: 'https://www.pre-onboarding-selection-task.shop',
   SIGNUP_SERVER_URL: '/auth/signup',
   SIGNIN_SERVER_URL: '/auth/signin',
   TODOS_SERVER_URL: '/todos',
 };
 
-export const { BASE_SERVER_URL, SIGNUP_SERVER_URL, SIGNIN_SERVER_URL, TODOS_SERVER_URL } = serverPath;
+export const { BASE_SERVER_URL, SIGNUP_SERVER_URL, SIGNIN_SERVER_URL, TODOS_SERVER_URL } = SERVER_PATH;
