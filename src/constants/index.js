ade212b257977c88ce983502db35ab865d7ff019
5kdk <tylerk1121@gmail.com>
2023-06-14 01:26:24 +0900

    feat: 서버 path 상수 생성

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,7 @@ export { ROOT_URL } from './path';
 export { SIGNIN_URL } from './path';
 export { SIGNUP_URL } from './path';
 export { TODO_URL } from './path';
+export { BASE_SERVER_URL } from './serverPath';
+export { SIGNUP_SERVER_URL } from './serverPath';
+export { SIGNIN_SERVER_URL } from './serverPath';
+export { TODOS_SERVER_URL } from './serverPath';
---
+++ src/constants/serverPath.js
@@ -0,0 +1,8 @@
+const serverPath = {
+  BASE_SERVER_URL: 'https://www.pre-onboarding-selection-task.shop',
+  SIGNUP_SERVER_URL: '/auth/signup',
+  SIGNIN_SERVER_URL: '/auth/signin',
+  TODOS_SERVER_URL: '/todos',
+};
+
+export const { BASE_SERVER_URL, SIGNUP_SERVER_URL, SIGNIN_SERVER_URL, TODOS_SERVER_URL } = serverPath;
