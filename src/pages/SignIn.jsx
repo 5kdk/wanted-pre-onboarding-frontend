d7810583e5a8fea4afd0132e9df2e72ee0a81dd8
5kdk <tylerk1121@gmail.com>
2023-06-14 00:42:01 +0900

    feat: SignIn 페이지 컴포넌트 UI 구현

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,3 +1,49 @@
-const SignIn = () => <div>SignIn</div>
+import { useState } from 'react';
+import { rem, validate } from '../utils';
+import { Button, Container, Input, Label, SubTitle } from '../components';
+
+const SignIn = () => {
+  const [formValues, setFormValues] = useState({ email: '', password: '' });
+
+  const isDisabled = !validate(formValues);
+
+  const handleFormChange = e => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('hi');
+    // api 로직 추가
+  };
+
+  return (
+    <Container w={rem(400)}>
+      <SubTitle>로그인</SubTitle>
+      <form onSubmit={handleSubmit}>
+        <Label htmlFor="email">Email address</Label>
+        <Input
+          type="email"
+          name="email"
+          value={formValues.email}
+          data-testid="email-input"
+          onChange={handleFormChange}
+        />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          value={formValues.password}
+          data-testid="password-input"
+          onChange={handleFormChange}
+        />
+        <Button data-testid="signup-button" disabled={isDisabled}>
+          Sign In
+        </Button>
+      </form>
+    </Container>
+  );
+};
 
 export default SignIn;
