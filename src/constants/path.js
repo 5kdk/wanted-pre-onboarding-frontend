95cdbec6624f4b32c5b3d42930cde8bf8b227d91
5kdk <tylerk1121@gmail.com>
2023-06-14 01:13:00 +0900

    fix: 네비게이션을 위한 url 수정

--- src/constants/path.js
+++ src/constants/path.js
@@ -1,8 +1,8 @@
 const PATHS = {
   ROOT_URL: '/',
-  SIGNIN_URL: 'signin',
-  SIGNUP_URL: 'signup',
-  TODO_URL: 'todo',
+  SIGNIN_URL: '/signin',
+  SIGNUP_URL: '/signup',
+  TODO_URL: '/todo',
 };
 
 export const { ROOT_URL, SIGNIN_URL, SIGNUP_URL, TODO_URL } = PATHS;
