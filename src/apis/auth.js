7c4c9322d0d5ee8bf223daeceac86987e5d24e17
5kdk <tylerk1121@gmail.com>
2023-06-14 02:59:29 +0900

    refactor: url + 연산자로 수정

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { BASE_SERVER_URL, SIGNIN_SERVER_URL, SIGNUP_SERVER_URL } from '../constants';
 
 export const signUp = async formValues => {
-  await axios.post(`${BASE_SERVER_URL}${SIGNUP_SERVER_URL}`, formValues);
+  await axios.post(`${BASE_SERVER_URL + SIGNUP_SERVER_URL}`, formValues);
 };
 
 export const signIn = async formValues => {
-  const { data } = await axios.post(`${BASE_SERVER_URL}${SIGNIN_SERVER_URL}`, formValues);
+  const { data } = await axios.post(`${BASE_SERVER_URL + SIGNIN_SERVER_URL}`, formValues);
   return data;
 };
