4d04347b78eb922a82bd7baab74ee9adad438a9f
5kdk <tylerk1121@gmail.com>
2023-06-13 20:01:58 +0900

    feat: GlobalStyle, ThemeProvider 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-const App = () => <div></div>;
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './styles';
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  fontSize: {
+    xs: '0.5rem',
+    sm: '0.75rem',
+    md: '1rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+  },
+  spacing: {
+    xs: '0.5rem',
+    sm: '0.75rem',
+    md: '1rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+  },
+  colors: {
+    primary: '#2b90d9',
+    white: '#ffffff',
+    black: '#282c37',
+  },
+};
+
+export default theme;
