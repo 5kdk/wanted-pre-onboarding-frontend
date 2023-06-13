e29125de9cb0facaf1e266df5d773e544d7cd268
5kdk <tylerk1121@gmail.com>
2023-06-13 23:27:49 +0900

    refactor: fontWeight, gray 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,23 +1,39 @@
+const fontSize = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  md: '1rem',
+  lg: '1.25rem',
+  xl: '1.5rem',
+};
+
+const fontWeight = {
+  xs: 100,
+  sm: 300,
+  md: 500,
+  lg: 700,
+  xl: 900,
+};
+
+const spacing = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  md: '1rem',
+  lg: '1.25rem',
+  xl: '1.5rem',
+};
+
+const colors = {
+  primary: '#2b90d9',
+  white: '#ffffff',
+  black: '#282c37',
+  gray: '#d9e1e8',
+};
+
 const theme = {
-  fontSize: {
-    xs: '0.5rem',
-    sm: '0.75rem',
-    md: '1rem',
-    lg: '1.25rem',
-    xl: '1.5rem',
-  },
-  spacing: {
-    xs: '0.5rem',
-    sm: '0.75rem',
-    md: '1rem',
-    lg: '1.25rem',
-    xl: '1.5rem',
-  },
-  colors: {
-    primary: '#2b90d9',
-    white: '#ffffff',
-    black: '#282c37',
-  },
+  fontSize,
+  fontWeight,
+  spacing,
+  colors,
 };
 
 export default theme;
