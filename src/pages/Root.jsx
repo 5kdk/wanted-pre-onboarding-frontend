c61808229d4ba6fff42e6033ba80bac00bf59520
5kdk <tylerk1121@gmail.com>
2023-06-13 21:34:14 +0900

    feat: Root 페이지 레이아웃

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,9 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Root = () => {
-  <div>
+const Container = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.spacing.xl};
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const Root = () => (
+  <Container>
+    <Title>원티드 프리온보딩 프론트엔드 - 선발 과제</Title>
     <Outlet />
-  </div>;
-};
+  </Container>
+);
 
 export default Root;
