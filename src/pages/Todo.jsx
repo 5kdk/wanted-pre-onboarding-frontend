b52c68c616e6df173a055b1159e63ed280b3d82d
5kdk <tylerk1121@gmail.com>
2023-06-14 14:01:23 +0900

    feat: sign out 기능 추가 및 sub title 추가

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,6 +1,8 @@
-import { Container, Loader, TodoInput, TodoItem } from '../components';
+import { Link } from 'react-router-dom';
+import { Button, Container, Loader, SubTitle, TodoInput, TodoItem } from '../components';
 import { useTodos } from '../hooks';
-import { rem } from '../utils';
+import { deleteToken, rem } from '../utils';
+import { ROOT_URL } from '../constants';
 
 const Todo = () => {
   const { todos, isLoading, error, create, update, remove } = useTodos();
@@ -8,17 +10,25 @@ const Todo = () => {
   if (isLoading) return <Loader />;
 
   return (
-    <Container w={rem(500)}>
-      <TodoInput create={create} />
-      {error && <div>{error.response.data.message}</div>}
-      {todos.length === 0 ? (
-        <div>아직 Todo가 없습니다.</div>
-      ) : (
-        todos.map(({ id, todo, isCompleted }) => (
-          <TodoItem key={id} id={id} todo={todo} isCompleted={isCompleted} update={update} remove={remove} />
-        ))
-      )}
-    </Container>
+    <>
+      <Container w={rem(500)}>
+        <SubTitle>Todo</SubTitle>
+        <TodoInput create={create} />
+        {error && <div>{error.response.data.message}</div>}
+        {todos.length === 0 ? (
+          <div>아직 Todo가 없습니다.</div>
+        ) : (
+          todos.map(({ id, todo, isCompleted }) => (
+            <TodoItem key={id} id={id} todo={todo} isCompleted={isCompleted} update={update} remove={remove} />
+          ))
+        )}
+      </Container>
+      <Container w={rem(500)} mt="lg">
+        <Link to={ROOT_URL} onClick={deleteToken}>
+          <Button type="button">Sign out</Button>
+        </Link>
+      </Container>
+    </>
   );
 };
 
