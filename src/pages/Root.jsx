a49cdd62b1b36aa9114a6056531702b093a87169
5kdk <tylerk1121@gmail.com>
2023-06-14 13:15:03 +0900

    feat: 새로고침시 로그인, 현재 경로에 따른 리다이렉팅 구현

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,22 +1,41 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import { getToken } from '../utils';
+import { SIGNIN_URL, SIGNUP_URL, TODO_URL } from '../constants';
 
 const Container = styled.div`
   text-align: center;
   margin: 0 auto;
-  padding: ${({ theme }) => theme.spacing.xl};
+  padding: ${props => props.theme.spacing.xl};
 `;
 
 const Title = styled.h1`
-  margin: ${({ theme }) => theme.spacing.xl} 0;
-  color: ${({ theme }) => theme.colors.black};
+  margin: ${props => props.theme.spacing.xl} 0;
+  color: ${props => props.theme.colors.black};
 `;
 
-const Root = () => (
-  <Container>
-    <Title>Wanted Pre-onboarding</Title>
-    <Outlet />
-  </Container>
-);
+const Root = () => {
+  const isLogin = getToken();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (isLogin) {
+      navigate(TODO_URL);
+    } else if (pathname === SIGNIN_URL) {
+      navigate(SIGNIN_URL);
+    } else {
+      navigate(SIGNUP_URL);
+    }
+  }, [isLogin, navigate, pathname]);
+
+  return (
+    <Container>
+      <Title>Wanted Pre-onboarding</Title>
+      <Outlet />
+    </Container>
+  );
+};
 
 export default Root;
